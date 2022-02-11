6879cc6624b328beb840fac8b94655aa96fa772f
Brice Duke <bricewduke@gmail.com>
2022-02-10 21:48:51 -0600

    chore(global): initialize app

---
+++ app/store.ts
@@ -0,0 +1,13 @@
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+
+const rootReducer = combineReducers({});
+
+export const store = configureStore({
+	reducer: {},
+	middleware: (getDefaultMiddleware) =>
+		getDefaultMiddleware({ serializableCheck: false }),
+});
+
+export type RootState = ReturnType<typeof rootReducer>;
+
+export type AppDispatch = typeof store.dispatch;
